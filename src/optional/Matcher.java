313c559511b1aa5151b96ae6dd751d95cf166584
jelenam86 <jelena.mat86@gmail.com>
2019-09-29 20:46:54 +0200

    Refactor Matcher class

--- src/optional/Matcher.java
+++ src/optional/Matcher.java
@@ -1,24 +1,24 @@
 package optional;
 
-/** Refactor this code. Use the MatcherTest to make sure you haven't broken anything. */
+/**
+ * Refactor this code. Use the MatcherTest to make sure you haven't broken
+ * anything.
+ */
 
 public class Matcher {
+
 	public Matcher() {
 	}
 
 	public boolean match(int[] expected, int[] actual, int clipLimit, int delta) {
-		// Clip "too-large" values
 		clip(actual, clipLimit);
+		return actual.length == expected.length && isInDelta(expected, actual, delta);
+	}
 
-		// Check for length differences
-		if (actual.length != expected.length)
-			return false;
-
-		// Check that each entry within expected +/- delta
+	private boolean isInDelta(int[] expected, int[] actual, int delta) {
 		for (int i = 0; i < actual.length; i++)
 			if (Math.abs(expected[i] - actual[i]) > delta)
 				return false;
-
 		return true;
 	}
 
@@ -27,5 +27,4 @@ public class Matcher {
 			if (actual[i] > clipLimit)
 				actual[i] = clipLimit;
 	}
-
 }
