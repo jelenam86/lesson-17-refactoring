1a4455bc80aaea8adbd584f3423546039fea53af
jelenam86 <jelena.mat86@gmail.com>
2019-09-29 20:42:13 +0200

    Refactoring Ella's game

--- src/exercises/ella/SideBar.java
+++ src/exercises/ella/SideBar.java
@@ -1,4 +1,5 @@
 package exercises.ella;
+
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
@@ -6,6 +7,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.lang.reflect.Field;
 
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -16,8 +18,8 @@ public class SideBar extends JPanel implements ActionListener, KeyListener {
 	JLabel snakeName = new JLabel("snake");
 	JLabel ringName = new JLabel("ring");
 	JLabel backpackName = new JLabel("backpack");
-	JLabel sparkleName = new JLabel("sparkles");
-	JLabel pawprintName = new JLabel("pawprints");
+	JLabel sparklesName = new JLabel("sparkles");
+	JLabel pawprintsName = new JLabel("pawprints");
 	JLabel diamondName = new JLabel("diamond");
 	JLabel balloonName = new JLabel("balloon");
 	JLabel appleName = new JLabel("apple");
@@ -28,7 +30,7 @@ public class SideBar extends JPanel implements ActionListener, KeyListener {
 	JLabel earthName = new JLabel("earth");
 	JLabel cactusName = new JLabel("cactus");
 	JLabel timeName = new JLabel("Time: 0");
-		
+
 	int foundMeX = 76;
 	int foundMeWidth = 275;
 	int foundMeHeight = 75;
@@ -45,201 +47,85 @@ public class SideBar extends JPanel implements ActionListener, KeyListener {
 
 	SideBar() {
 		setPreferredSize(new Dimension(285, 1000));
-		// bar.setBounds(1800, 900, 500, 500);
 		add(barTitle);
 		barTitle.setFont(new Font("Serif", Font.PLAIN, 45));
 		setLayout(null);
 		barTitle.setBounds(84, 20, 200, 100);
-		objectNames();
+		showLevelOneNames();
 		setOpaque(true);
 		setBackground(Color.PINK);
 		repaint();
-
 	}
 
-	void objectNames() {
-
-		
-		add(snakeName);
-		snakeName.setFont(new Font("Serif", Font.PLAIN, 30));
-		snakeName.setBounds(102, findMeY, 100, 75);
-		add(ringName);
-		ringName.setFont(new Font("Serif", Font.PLAIN, 30));
-		ringName.setBounds(113, findMeY2, 100, 75);
-		add(backpackName);
-		backpackName.setFont(new Font("Serif", Font.PLAIN, 30));
-		backpackName.setBounds(83, findMeY3, 275, 75);
-		add(sparkleName);
-		sparkleName.setFont(new Font("Serif", Font.PLAIN, 30));
-		sparkleName.setBounds(90, findMeY4, 250, 75);
-		add(pawprintName);
-		pawprintName.setFont(new Font("Serif", Font.PLAIN, 30));
-		pawprintName.setBounds(83, findMeY5, 275, 75);
-		add(diamondName);
-		diamondName.setFont(new Font("Serif", Font.PLAIN, 30));
-		diamondName.setBounds(87, findMeY6, 250, 75);
-		add(balloonName);
-		balloonName.setFont(new Font("Serif", Font.PLAIN, 30));
-		balloonName.setBounds(95, findMeY7, 250, 75);
-		add(timeName);
-		timeName.setFont(new Font("Serif", Font.PLAIN, 30));
-		timeName.setBounds(95, 850, 250, 75);
-
+	private void addObjectName(JLabel label, int xBound, int yBound, int width) {
+		add(label);
+		label.setFont(new Font("Serif", Font.PLAIN, 30));
+		label.setBounds(xBound, yBound, width, 75);
 	}
 
-	void objectNamesLevelTwo() {
+	void showLevelOneNames() {
+		addObjectName(snakeName, 102, findMeY, 100);
+		addObjectName(ringName, 113, findMeY2, 100);
+		addObjectName(backpackName, 83, findMeY3, 275);
+		addObjectName(sparklesName, 90, findMeY4, 250);
+		addObjectName(pawprintsName, 83, findMeY5, 275);
+		addObjectName(diamondName, 87, findMeY6, 250);
+		addObjectName(balloonName, 95, findMeY7, 250);
+		addObjectName(timeName, 95, 850, 250);
+	}
 
-		add(appleName);
-		appleName.setFont(new Font("Serif", Font.PLAIN, 30));
-		appleName.setBounds(102, findMeY, 100, 75);
-		add(bowName);
-		bowName.setFont(new Font("Serif", Font.PLAIN, 30));
-		bowName.setBounds(109, findMeY2, 90, 75);
-		add(roseName);
-		roseName.setFont(new Font("Serif", Font.PLAIN, 30));
-		roseName.setBounds(110, findMeY3, 100, 75);
-		add(cloudName);
-		cloudName.setFont(new Font("Serif", Font.PLAIN, 30));
-		cloudName.setBounds(101, findMeY4, 100, 75);
-		add(chickenName);
-		chickenName.setFont(new Font("Serif", Font.PLAIN, 30));
-		chickenName.setBounds(92, findMeY5, 250, 75);
-		add(earthName);
-		earthName.setFont(new Font("Serif", Font.PLAIN, 30));
-		earthName.setBounds(101, findMeY6, 100, 75);
-		add(cactusName);
-		cactusName.setFont(new Font("Serif", Font.PLAIN, 30));
-		cactusName.setBounds(101, findMeY7, 100, 75);
-
+	void showLevelTwoNames() {
+		addObjectName(appleName, 102, findMeY, 100);
+		addObjectName(bowName, 109, findMeY2, 90);
+		addObjectName(roseName, 110, findMeY3, 100);
+		addObjectName(cloudName, 101, findMeY4, 100);
+		addObjectName(chickenName, 92, findMeY5, 250);
+		addObjectName(earthName, 101, findMeY6, 100);
+		addObjectName(cactusName, 101, findMeY7, 100);
 	}
 
 	void nextLevelBar() {
 		currentBar += 1;
-		if (currentBar > BAR2) {
+		if (currentBar > BAR2)
 			currentBar = BAR1;
-		}
-		if (currentBar == BAR1) {
+		if (currentBar == BAR1)
 			setBackground(Color.PINK);
-
-		}
 		System.out.println(currentBar);
 		if (currentBar == 1) {
 			setBackground(Color.BLUE);
-			objectNamesLevelTwo();
+			showLevelTwoNames();
 		}
 		repaint();
-
 	}
 
-	void foundSparkles() {
-
-		sparkleName.setVisible(false);
-
+	void foundItem(String itemButton) {
+		for (Field f : getClass().getDeclaredFields())
+			if (f.getType() == JLabel.class && f.getName().substring(0, f.getName().length() - 4).equals(itemButton))
+				try {
+					JLabel item = (JLabel) f.get(this);
+					item.setVisible(false);
+				} catch (IllegalArgumentException | IllegalAccessException e) {
+					e.printStackTrace();
+				}
 	}
 
-	void foundRing() {
-
-		ringName.setVisible(false);
-
-	}
-
-	void foundBackpack() {
-
-		backpackName.setVisible(false);
-
-	}
-
-	void foundPawprint() {
-
-		pawprintName.setVisible(false);
-
-	}
-
-	void foundDiamond() {
-
-		diamondName.setVisible(false);
-
-	}
-
-	void foundSnake() {
-
-		snakeName.setVisible(false);
-
-	}
-
-	void foundBalloon() {
-
-		balloonName.setVisible(false);
-
-	}
-
-	void foundApple() {
-
-		appleName.setVisible(false);
-
-	}
-
-	void foundBow() {
-
-		bowName.setVisible(false);
-
-	}
-
-	void foundRose() {
-
-		roseName.setVisible(false);
-
-	}
-
-	void foundCloud() {
-
-		cloudName.setVisible(false);
-
-	}
-	
-	void foundChicken() {
-		
-		chickenName.setVisible(false);
-		
-	}
-	void foundEarth() {
-		
-		earthName.setVisible(false);
-		
-	}
-	void foundCactus() {
-		
-		cactusName.setVisible(false);
-		
-	}
 	void showTime(int time) {
-		
-
-	timeName.setText("Time: " + time);
-		
+		timeName.setText("Time: " + time);
 	}
 
 	@Override
 	public void keyTyped(KeyEvent e) {
-		// TODO Auto-generated method stub
-
 	}
 
 	@Override
 	public void keyPressed(KeyEvent e) {
-		// TODO Auto-generated method stub
-
 	}
 
 	@Override
 	public void keyReleased(KeyEvent e) {
-		// TODO Auto-generated method stub
-
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		// TODO Auto-generated method stub
-
 	}
 }
